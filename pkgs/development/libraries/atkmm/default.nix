8a7d699e5abc47eaabae243d38ff83760d9d5b99
Tor Hedin Brønner <torhedinbronner@gmail.com>
2019-03-07 17:18:25 +0100

    atkmm: 2.28.0 -> 2.29.1

--- pkgs/development/libraries/atkmm/default.nix
+++ pkgs/development/libraries/atkmm/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "atkmm";
-  version = "2.28.0";
+  version = "2.29.1";
 
   src = fetchurl {
     url = "mirror://gnome/sources/${pname}/${stdenv.lib.versions.majorMinor version}/${pname}-${version}.tar.xz";
-    sha256 = "0fnxrspxkhhbrjphqrpvl3zjm66n50s4cywrrrwkhbflgy8zqk2c";
+    sha256 = "0igm7a2hfzknyb02zbydidbkwpk53af965wba468w7lrqyivlh21";
   };
 
   outputs = [ "out" "dev" ];
